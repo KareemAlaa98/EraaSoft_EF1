79bbb1c4a1c7e64d2654dc11fd1d31095f30644c
kareem alaa <kareemalaanaga98@gmail.com>
2024-02-20 16:36:08 +0200

    first commit

---
+++ Task/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Configurations;
+using Task.Models;
+
+
+namespace Task
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public DbSet<TaskEntity> Tasks { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=EF1_Task;Integrated Security=True;TrustServerCertificate=True");
+        }
+         
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        /**** Check The Configuration Folder for the FluentAPI Code (Separation of Concerns) ****/
+            new TaskEntityTypeConfiguration().Configure(modelBuilder.Entity<TaskEntity>());
+
+
+            // Make Date column Required
+            // modelBuilder.Entity<TaskEntity>().Property(e => e.Date).IsRequired();
+
+            // Change table name to NewTask
+            // modelBuilder.Entity<TaskEntity>().ToTable("NewTask");
+
+            // Change the Date Column's name to Deadline
+            // modelBuilder.Entity<TaskEntity>().Property(e => e.Date).HasColumnName("Deadline");
+        }
+    }
+}
---
+++ Task/Configurations/TaskEntityTypeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Models;
+
+namespace Task.Configurations
+{
+    internal class TaskEntityTypeConfiguration : IEntityTypeConfiguration<TaskEntity>
+    {
+        public void Configure(EntityTypeBuilder<TaskEntity> builder)
+        {
+            // Make Date column Required
+            builder.Property(e => e.Date).IsRequired();
+
+            // Change table name to NewTask
+            builder.ToTable("NewTask");
+
+            // Change the Date Column's name to Deadline
+            builder.Property(e => e.Date).HasColumnName("Deadline");
+        }
+    }
+}
